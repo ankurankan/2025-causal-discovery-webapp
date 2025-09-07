baa05c5e4d248412370a5389758c31dc3614665c
Ankur Ankan <ankurankan@gmail.com>
2025-09-07 16:36:37 +0200

    Add links to example datasets

--- utils.js
+++ utils.js
@@ -18,12 +18,11 @@ let varTypes = {};
 // Placeholder mapping for future example dataset URLs; to be filled later by user.
 // Keys must match values used in #example_dataset_select options.
 const EXAMPLE_DATASET_URLS = {
-  example1: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/mediator.csv', // TODO: set to full CSV URL
-  example2: '', // TODO: set to full CSV URL
-  example3: '',  // TODO: set to full CSV URL
-  example4: '',
-  example5: '',
-  example6: ''
+  example1: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/mediator.csv',
+  example2: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/asia.csv',
+  example3: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/cancer.csv',
+  example4: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/alarm.csv',
+  example5: 'https://raw.githubusercontent.com/ankurankan/2025-causal-discovery-webapp/refs/heads/new_features/examples/ecoli70.csv'
 };
 
 // Ensure a status element exists (created lazily)
